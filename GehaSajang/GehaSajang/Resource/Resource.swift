bb3da8a3fb3ad37f5589a87386f06bc075a88d72
dev-Lena <keunnalee@gmail.com>
2020-08-17 00:00:05 +0900

    add Resource file

---
+++ GehaSajang/GehaSajang/Resource/Resource.swift
@@ -0,0 +1,29 @@
+//
+//  Resource.swift
+//  GehaSajang
+//
+//  Created by Keunna Lee on 2020/08/16.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import Foundation
+
+struct Resource {
+    let name:String
+    let fileType: String
+}
+
+enum ResourceName {
+    static let loginBackgroundVideo = "GehaSaJangLoginBackgroundVideo"
+    static let blackLogo = "gehaSaJangLogo_black"
+    static let bigWhiteLogo = "gehaSaJangLogo_white_big"
+    static let whiteLogo = "gehaSaJangLogo_white"
+}
+
+enum ResourceType {
+    static let mp4 = ".mp4"
+}
+
+enum ColorName {
+    static let mainBlue = "MainBlue"
+}
