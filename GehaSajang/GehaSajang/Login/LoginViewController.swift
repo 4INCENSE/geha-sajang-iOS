7e73fb101e6def8f87f80b4d50fd4c071de8cd68
dev-Lena <keunnalee@gmail.com>
2020-08-28 02:54:19 +0900

    delete unnecessary methods

--- GehaSajang/GehaSajang/Login/LoginViewController.swift
+++ GehaSajang/GehaSajang/Login/LoginViewController.swift
@@ -44,18 +44,6 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        let optionMenu = UIAlertController(title: nil, message: "사용자 유형을 선택해 주세요", preferredStyle: .actionSheet)
-        
-        let deleteAction = UIAlertAction(title: "사장님으로 시작하기", style: .default)
-        let saveAction = UIAlertAction(title: "스탭으로 시작하기", style: .default)
-        
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
-        
-        optionMenu.addAction(deleteAction)
-        optionMenu.addAction(saveAction)
-        optionMenu.addAction(cancelAction)
-        
-        self.present(optionMenu, animated: true, completion: nil)
     }
 }
 
