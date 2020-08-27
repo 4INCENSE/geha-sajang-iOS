3089667193d35c243a732aaed8d8d78da5a2fad8
dev-Lena <keunnalee@gmail.com>
2020-08-28 02:55:55 +0900

    create TermsVC and SignUpVC

---
+++ GehaSajang/SignUp/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  GehaSajang
+//
+//  Created by Keunna Lee on 2020/08/28.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
