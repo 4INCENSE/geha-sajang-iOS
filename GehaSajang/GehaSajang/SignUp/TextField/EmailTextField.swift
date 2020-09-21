3469650ce4af001c159f31964fd1749abad16770
dev-Lena <keunnalee@gmail.com>
2020-09-21 23:51:47 +0900

    #17 refactor: separate TextFieldPlaceholderMessage enum

--- GehaSajang/GehaSajang/SignUp/TextField/EmailTextField.swift
+++ GehaSajang/GehaSajang/SignUp/TextField/EmailTextField.swift
@@ -8,13 +8,6 @@
 
 import UIKit
 
-enum TextFieldPlaceholderMessage {
-    static let EmailTextFieldMessage = "이메일을 입력하주세요"
-    static let PasswordTextFieldMessage = "영문, 숫자 조합으로 입력해주세요(8-16자)"
-    static let PasswordChekcTextFieldMessage = "비밀번호를 입력해주세요"
-    static let NicknameTextFieldMessage = "2-10자로 입력해주세요"
-}
-
 class EmailTextField: TextFieldObserver {
     
     deinit {
