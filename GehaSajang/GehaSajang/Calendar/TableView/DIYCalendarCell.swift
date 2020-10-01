fcea359bb167063fc8a030d81a134ef922d6e529
dev-Lena <keunnalee@gmail.com>
2020-10-02 01:05:36 +0900

    #21 feat: add tableViewCell classes
    - enum: TableViewIndexPath
    - cell class: TransitionButtonsRowTableViewCell, DIYCalendarCell

---
+++ GehaSajang/GehaSajang/Calendar/TableView/DIYCalendarCell.swift
@@ -0,0 +1,23 @@
+//
+//  DIYCalendarCell.swift
+//  GehaSajang
+//
+//  Created by Keunna Lee on 2020/09/28.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class DIYCalendarCell: FSCalendarCell {
+    
+    required init!(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+    
+}
---
+++ GehaSajang/GehaSajang/Calendar/TableView/TableViewIndexPath.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewIndexPath.swift
+//  GehaSajang
+//
+//  Created by Keunna Lee on 2020/10/02.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+import Foundation
+
+enum TableViewIndexPath {
+    static let numberOfSections = 2
+    static let lastRow = 11
+    static let numberOfRowsInEachSection = [1, lastRow]
+    static let firstSection = 0
+    static let secondSection = 1
+}
