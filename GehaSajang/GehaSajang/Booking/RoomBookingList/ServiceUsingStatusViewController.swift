845308cbe86cf0d4dca6e93aba62b71b1589bcd3
dev-Lena <keunnalee@gmail.com>
2020-10-02 01:07:42 +0900

    #21 feat: create class and xib file for ServiceUsingStatusViewController

---
+++ GehaSajang/GehaSajang/Booking/RoomBookingList/ServiceUsingStatusViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ServiceUsingStatusViewController.swift
+//  GehaSajang
+//
+//  Created by Keunna Lee on 2020/09/30.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class ServiceUsingStatusViewController: UIViewController {
+
+    //TODO: API에서 서비스 이용 현황 요청 후 받아온 데이터를 TableView로 표시
+    //TODO: TableView 안에 CollectionView
+    //TODO: TabkeView Section을 터치하면 해당
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
